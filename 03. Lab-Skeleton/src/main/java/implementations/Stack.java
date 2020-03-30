b65ff9bb767704b29e7ed5ba2edf4974913ef90c
deiom <46797594+deiom@users.noreply.github.com>
2020-03-30 19:41:23 +0300

    Add files via upload

---
+++ 03. Lab-Skeleton/src/main/java/implementations/Stack.java
@@ -0,0 +1,85 @@
+package implementations;
+
+import interfaces.AbstractStack;
+
+import java.util.Iterator;
+
+public class Stack<E> implements AbstractStack<E> {
+    private Node<E> top;
+    private int size;
+
+    private static class Node<E>{
+
+    private E value;
+    private Node<E> nexElement;
+    Node(E element){
+        this.value = element;
+     }
+    }
+
+    public Stack(){
+        this.top = null;
+        this.size = 0;
+    }
+
+    @Override
+    public void push(E element) {
+        Node<E> newElement = new Node<>(element);
+
+        newElement.nexElement = this.top;
+        this.top = newElement;
+
+        this.size++;
+    }
+
+    @Override
+    public E pop() {
+        ensureNonEmpty();
+
+        Node<E> tmp = this.top;
+        this.top = tmp.nexElement;
+        this.size--;
+        return  tmp.value;
+    }
+
+    @Override
+    public E peek() {
+        ensureNonEmpty();
+
+        return this.top.value;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private Node<E> current = top;
+            @Override
+            public boolean hasNext() {
+                return this.current.nexElement != null;
+            }
+
+            @Override
+            public E next() {
+                E value = this.current.value;
+                this.current = this.current.nexElement;
+                return value;
+            }
+        };
+    }
+
+    private void ensureNonEmpty(){
+        if (this.isEmpty()){
+            throw new IllegalStateException("No elements in this stack");
+        }
+    }
+}
